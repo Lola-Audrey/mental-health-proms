0ddb0bfccabf8ed639761820520e132f2c9259b5
Milan Wiedemann <milan.wiedemann@gmail.com>
2026-03-26 11:23:08 +0000

    Add variables to dataset definition earlier

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -55,25 +55,22 @@ invalid_gad7_scores = gad7_score_event.where(
 )
 
 # PROMs-related patient counts
-count_phq9_score = phq9_score_event.count_for_patient()
-count_phq9_procedure = phq9_procedure_event.count_for_patient()
-count_gad7_score = gad7_score_event.count_for_patient()
-count_gad7_procedure = gad7_procedure_event.count_for_patient()
-count_all_proms_score = count_phq9_score + count_gad7_score
-count_all_proms_procedure = count_phq9_procedure + count_gad7_procedure
+dataset.count_phq9_score = phq9_score_event.count_for_patient()
+dataset.count_phq9_procedure = phq9_procedure_event.count_for_patient()
+dataset.count_gad7_score = gad7_score_event.count_for_patient()
+dataset.count_gad7_procedure = gad7_procedure_event.count_for_patient()
+dataset.count_all_proms_score = dataset.count_phq9_score + dataset.count_gad7_score
+dataset.count_all_proms_procedure = dataset.count_phq9_procedure + dataset.count_gad7_procedure
 
-count_phq9_out_of_range = invalid_phq9_scores.count_for_patient()
-count_gad7_out_of_range = invalid_gad7_scores.count_for_patient()
+dataset.count_phq9_out_of_range = invalid_phq9_scores.count_for_patient()
+dataset.count_gad7_out_of_range = invalid_gad7_scores.count_for_patient()
 
 # PROMs flags
-has_phq9_score = count_phq9_score > 0
-has_gad7_score = count_gad7_score > 0
-has_phq9_procedure = count_phq9_procedure > 0
-has_gad7_procedure = count_gad7_procedure > 0
-has_any_proms_score = has_phq9_score | has_gad7_score
-
-# Define dataset variables
-dataset.define_population(has_registration & is_16_or_older & is_alive)
+dataset.has_phq9_score = dataset.count_phq9_score > 0
+dataset.has_gad7_score = dataset.count_gad7_score > 0
+dataset.has_phq9_procedure = dataset.count_phq9_procedure > 0
+dataset.has_gad7_procedure = dataset.count_gad7_procedure > 0
+dataset.has_any_proms_score = dataset.has_phq9_score | dataset.has_gad7_score
 
 # Demographic variables
 dataset.sex = patients.sex
@@ -89,13 +86,5 @@ dataset.latest_ethnicity_group = (
     .snomedct_code.to_category(ethnicity_cod)
 )
 
-# PROMs variables
-dataset.has_any_proms_score = has_any_proms_score
-dataset.count_phq9_score = count_phq9_score
-dataset.count_gad7_score = count_gad7_score
-dataset.count_all_proms_score = count_all_proms_score
-dataset.count_all_proms_procedure = count_all_proms_procedure
-dataset.count_phq9_out_of_range = count_phq9_out_of_range
-dataset.count_gad7_out_of_range = count_gad7_out_of_range
-dataset.count_phq9_procedure = count_phq9_procedure
-dataset.count_gad7_procedure = count_gad7_procedure
+# Define population
+dataset.define_population(has_registration & is_16_or_older & is_alive)
